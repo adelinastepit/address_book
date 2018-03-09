a510207c3331e61d14cd5736c45ed837764bbfec
adelinastepit <36442978+adelinastepit@users.noreply.github.com>
2018-03-09 12:09:26 +0200

    Add create table script

---
+++ dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE TABLE [dbo].[tbl_users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[lastname] [nchar](100) NULL,
+	[firstname] [nchar](100) NULL,
+	[mobile] [nchar](15) NULL,
+	[telephone] [nchar](15) NULL,
+	[email] [nchar](100) NULL,
+ CONSTRAINT [PK_tbl_users] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
